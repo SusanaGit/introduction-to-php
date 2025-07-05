4a3f6c24c3217ed57064bb9bc04cecadd96be705
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-07-05 13:34:08 +0200

    add 0003-show-add-different-variables.php exercise

---
+++ 0003-show-add-different-variables/0003-show-add-different-variables.php
@@ -0,0 +1,8 @@
+<?php
+$var1 = 3; // variable numérica
+$var2 = '7'; // variable de texto con contenido numérico
+$var3 = 'b'; // variable de texto con contenido texto
+echo $var1+$var2; // resultado 10
+echo '<br>';
+echo $var1*$var3; // error de incompatibilidad de tipos
+?>
