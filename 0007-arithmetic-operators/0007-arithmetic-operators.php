612e4cc6413cb3f78ca17505add084fae82ba688
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-07-06 09:37:14 +0200

    add 0007-arithmetic-operators.php

---
+++ 0007-arithmetic-operators/0007-arithmetic-operators.php
@@ -0,0 +1,11 @@
+<?php
+echo "<br/> adición " . (7+2); // 9
+
+echo "<br/> sustracción " . (7-2); // 5
+
+echo "<br/> multiplicación " . (7*2); // 14
+
+echo "<br/> división " . (7/2); // 3.5
+
+echo "<br/> resto " . (7%2); // 1
+?>
