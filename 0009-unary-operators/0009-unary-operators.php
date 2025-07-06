71c0eb3f4a2922c678caa14188691a52eebc3d27
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-07-06 10:36:45 +0200

    add 0009-unary-operators.php

---
+++ 0009-unary-operators/0009-unary-operators.php
@@ -0,0 +1,16 @@
+<?php
+$a = 2;
+$c = -$a; // $c = -2 , equivale a multiplicar por -1
+echo $c . "<br>";
+
+$b = -2;
+$c = -$b; // $c = 2
+echo $c . "<br>";
+
+$a = 10;
+$b = 10;
+$a++;
+$b--;
+echo $a . "<br>"; // equivale a $a = $a + 1
+echo $b . "<br>"; // equivale a $b = $b - 1
+?>
