973fa89f4d5e4b538dd1f467a4d85376b917e326
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-07-08 20:14:29 +0200

    add 0013-form-exercise

---
+++ 0013-form-exercise/0013-form-exercise.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ca">
+    <head>
+        <meta charset="UTF-8">
+        <title>0013-form-exercise</title>
+        <link rel="stylesheet" href="0013-form-exercise.css">
+    </head>
+    <form method="get" action="0013-show-data.php">
+        <label>
+            <textarea name="info"></textarea>
+        </label>
+        <br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</html>
