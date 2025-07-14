ce25511ffa978fe5a89adcdc8d62e50bf2a443a4
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-07-14 19:02:21 +0200

    0015-PLA02-exercise finished

--- 0015-PLA02-exercise/PLA02_Coste_Hotel.php
+++ 0015-PLA02-exercise/PLA02_Coste_Hotel.php
@@ -1,21 +1,15 @@
 <?php
-	// CONSTANTES
 
-    // inicializar las variables que utilicemos en el documento html
     $noches = 0;
     $ciudad = '';
     $coche = 0;
     $total = 0;
     $errores = [];
 
-    // datos que tiene el servidor
-    // print_r: para visualizar contenido de un array
-    // las claves coinciden con el valor que hemos dado a los atributos name del formulario
-    print_r($_POST);
+    //print_r($_POST);
 
-    // ver si existe el índice del array del servidor 'enviar'
     if (isset($_POST['enviar'])) {
-        // recoger los tres datos del formulario (noches, ciudad, coche)
+
         try {
             // validación de datos
             $noches = $_POST['noches'] ?? null;
@@ -44,12 +38,10 @@
         }
     }
 
-    // devolver el precio total del hotel, considerando que cada noche cuesta 60 euros
     function costeHotel(int $noches) : int{
         return $noches * 60;
     }
 
-    // devolver el precio del avión según la ciudad
     function costeCiudad($ciudad) : int{
         switch ($ciudad) {
             case 'Madrid':
@@ -65,7 +57,6 @@
         }
     }
 
-    // devolver el precio del alquiler del coche
     function costeCoche(int $dias): int {
         $alquilercoche = $dias * 40;
 
@@ -81,63 +72,63 @@
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>PLA02</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-</head>
-<body>
-	<main>
-		<h1 class='centrar'>PLA02: COSTE HOTEL</h1>
-		<br>
-		<form method="post" action="#">
-			<div class="row mb-3">
-			    <label for="noches" class="col-sm-3 col-form-label">Número de noches:</label>
-			    <div class="col-sm-9">
-			      <input type="number" class="form-control" name="noches" id="noches">
-			    </div>
-			</div>
-			<div class="row mb-3">
-			    <label for="ciudad" class="col-sm-3 col-form-label">Destino:</label>
-			    <div class="col-sm-9">
-					<select class="form-select" name='ciudad'>
-					  	<option selected value=''>Selecciona un destino</option>
-					  	<option>Madrid</option>
-						<option>Paris</option>
-						<option>Los Angeles</option>
-						<option>Roma</option>
-					</select>
-				</div>
-			</div>
-			<div class="row mb-3">
-			    <label for="coche" class="col-sm-3 col-form-label">Días alquiler coche:</label>
-			    <div class="col-sm-9">
-			      <input type="number" class="form-control" name="coche" id="coche">
-			    </div>
-			</div>
-			<label class="col-sm-3 col-form-label"></label>
-		  	<button type="submit" class="btn btn-primary" name='enviar'>Enviar datos</button>
-            <button type="reset" class="btn btn-primary">Limpiar</button>
-            <br><br>
-		  	<div class="row mb-3">
-			    <label class="col-sm-3 col-form-label">Coste total: </label>
-			    <div class="col-sm-9">
-			      <input type="text" class="form-control" name="total" id="total" disabled
-                  value="<?php echo $total; ?>">
-			    </div>
-			</div><br>
-			<span class='errores'>
-                <?php
-                    for ($i = 0; $i < count($errores); $i++) {
-                        echo $errores[$i];
-                        echo "<br>";
-                    }
-                ?>
-                <br>
-            </span>
-		</form>
-	</main>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>PLA02</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+    <body>
+        <main>
+            <h1 class='centrar'>PLA02: COSTE HOTEL</h1>
+            <br>
+            <form method="post" action="#">
+                <div class="row mb-3">
+                    <label for="noches" class="col-sm-3 col-form-label">Número de noches:</label>
+                    <div class="col-sm-9">
+                      <input type="number" class="form-control" name="noches" id="noches">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="ciudad" class="col-sm-3 col-form-label">Destino:</label>
+                    <div class="col-sm-9">
+                        <select class="form-select" name='ciudad'>
+                            <option selected value=''>Selecciona un destino</option>
+                            <option>Madrid</option>
+                            <option>Paris</option>
+                            <option>Los Angeles</option>
+                            <option>Roma</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="coche" class="col-sm-3 col-form-label">Días alquiler coche:</label>
+                    <div class="col-sm-9">
+                      <input type="number" class="form-control" name="coche" id="coche">
+                    </div>
+                </div>
+                <label class="col-sm-3 col-form-label"></label>
+                <button type="submit" class="btn btn-primary" name='enviar'>Enviar datos</button>
+                <button type="submit" class="btn btn-primary">Limpiar</button>
+                <br><br>
+                <div class="row mb-3">
+                    <label class="col-sm-3 col-form-label">Coste total: </label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="total" id="total" disabled
+                      value="<?php echo $total; ?>">
+                    </div>
+                </div><br>
+                <span class='errores'>
+                    <?php
+                        for ($i = 0; $i < count($errores); $i++) {
+                            echo $errores[$i];
+                            echo "<br>";
+                        }
+                    ?>
+                    <br>
+                </span>
+            </form>
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
 </html>
