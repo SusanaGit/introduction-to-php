ad951e135f6cf9a584899c023af86cf91d23c8b2
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-07-18 22:33:38 +0200

    0016-PLA03-exercise add session start config

--- 0016-PLA03-exercise/index.php
+++ 0016-PLA03-exercise/index.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
+
+    # el fichero index.php es el primero que se va a ejecutar
+    # activo las variables de sesión
 	session_start();
+
+    # creo la variable de sesión, que hasta que no cerremos el navegador va a estar activa
+    $_SESSION['datospersonas'] = [];
+
+    # para cargar el archivo personas.php que es donde tenemos el formulario
+    header("Location: personas.php");
 
 	//eliminar datos de accesos anteriores
 	
